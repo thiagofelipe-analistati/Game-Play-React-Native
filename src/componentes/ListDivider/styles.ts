d264dbb3bb20b34be3a5bf40c6a4089b544ee61b
Thiago felipe <thiagofelipe_analistati@outlook.com>
2021-08-17 22:08:01 -0300

    Ajuste componente ListDivider

--- src/componentes/ListDivider/styles.ts
+++ src/componentes/ListDivider/styles.ts
@@ -3,10 +3,11 @@ import { theme } from "../../global/styles/them";
 
 export const styles = StyleSheet.create({
     container :{
-        width:  '80%',
+        width:  '78%',
         height: 1,
         backgroundColor: theme.color.secondary40,
-        marginVertical: 21,
+        marginVertical: 31,
+        marginTop: 2,
         alignSelf: 'flex-end'
     }
 })  
